f3d0d2255cfda3d6f39ae525cabfb0a2e31db4b9
TkChipChipLeader89 <jonathansotelo99@gmail.com>
2026-03-11 05:14:13 +0000

    Created Beverage & Methods

---
+++ src/main/java/edu/norcocollege/cis18b/week3/coffeeshop/Beverage.java
@@ -0,0 +1,38 @@
+//package
+package edu.norcocollege.cis18b.week3.coffeeshop;
+
+//imports
+import java.math.BigDecimal;
+
+//class for beverage menu item
+public class Beverage extends MenuItem{
+    //enum for beverage size
+    public enum Size {
+        SMALL, 
+        MEDIUM, 
+        LARGE
+    }
+    //Private variable for size
+    private Size size;
+    //Constructor
+    public Beverage(String sku, String name, BigDecimal basePrice, Size size){
+        super(sku, name, basePrice);
+        if(size==null){
+            throw new IllegalArgumentException("Size cannot be null!");
+        }
+        this.size = size;
+    }
+    //Size multiplier method
+    public BigDecimal getSizeMultiplier(){
+        switch(size){
+            case SMALL:
+                return new BigDecimal(1.00);
+            case MEDIUM:
+                return new BigDecimal(1.20);
+            case LARGE:
+                return new BigDecimal(1.40);
+            default:
+                throw new IllegalStateException("Unexpected size: " + size);
+        }
+    }    
+}
