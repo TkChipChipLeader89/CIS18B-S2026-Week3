10d127a871ec326e1d35589e6115b76ce1090c63
TkChipChipLeader89 <jonathansotelo99@gmail.com>
2026-03-11 05:26:49 +0000

    Created Latte Class & Methods

---
+++ src/main/java/edu/norcocollege/cis18b/week3/coffeeshop/Latte.java
@@ -0,0 +1,20 @@
+//package
+package edu.norcocollege.cis18b.week3.coffeeshop;
+
+//imports
+import java.math.BigDecimal;
+
+//class for latte menu item
+public class Latte extends Beverage{
+    //Base price for a latte
+    private static final BigDecimal BASE_PRICE = new BigDecimal("4.50");
+    //Constructor
+    public Latte(String sku, Size size){
+        super(sku, "Latte", BASE_PRICE, size);
+    }
+    //Override get price method to include size multiplier
+    @Override
+    public BigDecimal getPrice() {
+        return super.getPrice().multiply(getSizeMultiplier());
+    }
+}
