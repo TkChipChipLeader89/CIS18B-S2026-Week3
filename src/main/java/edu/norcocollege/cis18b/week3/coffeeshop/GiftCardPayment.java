cf22a4c4cb9874bc5ca044448d315b9774b71478
TkChipChipLeader89 <jonathansotelo99@gmail.com>
2026-03-11 06:24:29 +0000

    Created Class GiftCardPayment & Methods

---
+++ src/main/java/edu/norcocollege/cis18b/week3/coffeeshop/GiftCardPayment.java
@@ -0,0 +1,32 @@
+//package
+package edu.norcocollege.cis18b.week3.coffeeshop;
+
+//imports
+import java.math.BigDecimal;
+import java.time.Instant;
+
+// class GiftCardPayment implements PaymentMethod
+public class GiftCardPayment implements PaymentMethod{
+    //Variable to store balance
+    private BigDecimal balance;
+    //Constuctor
+    public GiftCardPayment(BigDecimal balance) {
+        if (balance == null || balance.compareTo(BigDecimal.ZERO) < 0) {
+            throw new IllegalArgumentException("Balance must be greater or equal to 0!");
+        }
+        this.balance = balance;
+    }
+    //Overide pay method to process gift card payment
+    @Override
+    public PaymentReceipt pay(String orderId, BigDecimal amount) {
+        if (balance.compareTo(amount) < 0) {
+            throw new IllegalArgumentException("Insufficient balance on gift card!");
+        }
+        balance = balance.subtract(amount);
+        return new PaymentReceipt(orderId, amount, "Gift Card", Instant.now());
+    }
+    // Getter for balance
+    public BigDecimal getBalance() {
+        return balance;
+    }
+}
